533faeccb4b1c31104b965db3de6e5c5900af32d
Dante Cantal <danteacantal@gmail.com>
2019-03-29 14:13:57 -0400

    Attempted to create navbar dropdown

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -592,45 +592,112 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
+/* harmony import */ var _navbar_dropdown__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./navbar_dropdown */ "./frontend/components/navbar/navbar_dropdown.jsx");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
-var NavBar = function NavBar(_ref) {
-  var currentUser = _ref.currentUser,
-      logout = _ref.logout;
 
-  var sessionLinks = function sessionLinks() {
-    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
-      className: "new-group-login-signup"
-    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      className: "new-group-link",
-      to: "/groups/new"
-    }, "Start a new group"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
-      className: "login-signup"
-    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      to: "/login"
-    }, "Login"), "\xA0\xA0\xA0\xA0", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      to: "/signup"
-    }, "Sign up")));
-  };
 
-  var personalGreeting = function personalGreeting() {
-    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hgroup", {
-      className: "header-group"
-    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-      className: "new-group-link",
-      to: "/groups/new"
-    }, "Start a new group"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-      className: "profile-pic",
-      src: "https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300"
-    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-      className: "header-button",
-      onClick: logout
-    }, "Log Out"));
-  };
+var NavBar =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(NavBar, _React$Component);
 
-  return currentUser ? personalGreeting() : sessionLinks();
-};
+  function NavBar(props) {
+    var _this;
+
+    _classCallCheck(this, NavBar);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(NavBar).call(this, props));
+    _this.renderDropdown = _this.renderDropdown.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(NavBar, [{
+    key: "renderDropdown",
+    value: function renderDropdown() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_navbar_dropdown__WEBPACK_IMPORTED_MODULE_2__["default"], null);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var sessionLinks = function sessionLinks() {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
+          className: "new-group-login-signup"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          className: "new-group-link",
+          to: "/groups/new"
+        }, "Start a new group"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
+          className: "login-signup"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/login"
+        }, "Login"), "\xA0\xA0\xA0\xA0", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/signup"
+        }, "Sign up")));
+      };
+
+      var personalGreeting = function personalGreeting() {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hgroup", {
+          className: "header-group"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          className: "new-group-link",
+          to: "/groups/new"
+        }, "Start a new group"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+          className: "profile-pic",
+          src: "https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300",
+          onClick: _this2.renderDropdown
+        }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+          className: "header-button",
+          onClick: _this2.props.logout
+        }, "Log Out"));
+      };
+
+      return this.props.currentUser ? personalGreeting() : sessionLinks();
+    }
+  }]);
+
+  return NavBar;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component); // const NavBar = ({ currentUser, logout }) => {
+//     const sessionLinks = () => (
+//         <nav className="new-group-login-signup">
+//             <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+//             <nav className="login-signup">
+//                 <Link to="/login">Login</Link>
+//                 &nbsp;&nbsp;&nbsp;&nbsp;
+//                 <Link to="/signup">Sign up</Link>
+//             </nav>
+//         </nav>
+//     );
+//     const personalGreeting = () => (
+//         <hgroup className="header-group">
+//             <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+//             {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
+//             <img className="profile-pic" src="https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300"></img>
+//             <button className="header-button" onClick={logout}>Log Out</button>
+//         </hgroup>
+//     );
+//     return currentUser ? personalGreeting() : sessionLinks();
+// };
+
 
 /* harmony default export */ __webpack_exports__["default"] = (NavBar);
 
@@ -667,6 +734,74 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(mapStateToProps, mapDispatchToProps)(_navbar__WEBPACK_IMPORTED_MODULE_2__["default"]));
+
+/***/ }),
+
+/***/ "./frontend/components/navbar/navbar_dropdown.jsx":
+/*!********************************************************!*\
+  !*** ./frontend/components/navbar/navbar_dropdown.jsx ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+ // import meetupvideo from '../../../app/assets/videos/meetupbanner.mp4';
+
+var NavbarDropdown =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(NavbarDropdown, _React$Component);
+
+  function NavbarDropdown(props) {
+    _classCallCheck(this, NavbarDropdown);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(NavbarDropdown).call(this, props));
+  }
+
+  _createClass(NavbarDropdown, [{
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "navbar-dropdown"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "dropdown-left"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+        className: "nav-account-groups"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Test Group 1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Test Group 2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Test Group 3"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "dropdown-right"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+        className: "nav-account-links"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Profile"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Settings"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Log Out"))));
+    }
+  }]);
+
+  return NavbarDropdown;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+
+/* harmony default export */ __webpack_exports__["default"] = (NavbarDropdown);
 
 /***/ }),
 
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -6,31 +6,71 @@ import {
     Link,
     HashRouter
 } from 'react-router-dom';
+ 
+import NavbarDropdown from './navbar_dropdown';
 
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.renderDropdown = this.renderDropdown.bind(this);
+    }
 
+    renderDropdown(){
+        return (
+            <NavbarDropdown />
+        );
+    }
 
-const NavBar = ({ currentUser, logout }) => {
-    const sessionLinks = () => (
-        <nav className="new-group-login-signup">
-            <Link className="new-group-link" to="/groups/new">Start a new group</Link>
-            <nav className="login-signup">
-                <Link to="/login">Login</Link>
-                &nbsp;&nbsp;&nbsp;&nbsp;
+    render() {
+        const sessionLinks = () => (
+            <nav className="new-group-login-signup">
+                <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+                <nav className="login-signup">
+                    <Link to="/login">Login</Link>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                 <Link to="/signup">Sign up</Link>
+                </nav>
             </nav>
-        </nav>
-    );
-    const personalGreeting = () => (
-        <hgroup className="header-group">
-            <Link className="new-group-link" to="/groups/new">Start a new group</Link>
-            {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
-            <img className="profile-pic" src="https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300"></img>
-            <button className="header-button" onClick={logout}>Log Out</button>
-        </hgroup>
-    );
+        );
+        const personalGreeting = () => (
+            <hgroup className="header-group">
+                <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+                {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
+                <img className="profile-pic" src="https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300" onClick={this.renderDropdown}></img>
+                <button className="header-button" onClick={this.props.logout}>Log Out</button>
+            </hgroup>
+        );
 
-    return currentUser ? personalGreeting() : sessionLinks();
-};
+        return this.props.currentUser ? personalGreeting() : sessionLinks();
+    }
+
+
+
+
+}
+
+// const NavBar = ({ currentUser, logout }) => {
+//     const sessionLinks = () => (
+//         <nav className="new-group-login-signup">
+//             <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+//             <nav className="login-signup">
+//                 <Link to="/login">Login</Link>
+//                 &nbsp;&nbsp;&nbsp;&nbsp;
+//                 <Link to="/signup">Sign up</Link>
+//             </nav>
+//         </nav>
+//     );
+//     const personalGreeting = () => (
+//         <hgroup className="header-group">
+//             <Link className="new-group-link" to="/groups/new">Start a new group</Link>
+//             {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
+//             <img className="profile-pic" src="https://secure.gravatar.com/avatar/b91e3eff4d79f02620073df23d39a8da?secure=true&size=300"></img>
+//             <button className="header-button" onClick={logout}>Log Out</button>
+//         </hgroup>
+//     );
+
+//     return currentUser ? personalGreeting() : sessionLinks();
+// };
 
 
 export default NavBar;
